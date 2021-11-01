e86fde2bed5be54c718a52c58ec4641cc409370c
tanzhiyu <615784818@qq.com>
2021-11-01 18:22:50 +0800

    package scripts

--- rollup.node.config.js
+++ rollup.node.config.js
@@ -2,14 +2,14 @@
  * @Author: tanzhiyu
  * @Date: 2021-11-01 17:18:58
  * @LastEditors: tanzhiyu
- * @LastEditTime: 2021-11-01 17:33:05
+ * @LastEditTime: 2021-11-01 18:05:23
  */
 import typescript from "@rollup/plugin-typescript";
 export default {
   input: "src/node/index.ts",
   output: {
-    file: "build/bundle.js",
-    format: "cjs"
+    dir: "build",
+    format: "umd"
   },
   plugins: [typescript()]
 }
--- src/node/utils/util.ts
+++ src/node/utils/util.ts
@@ -2,28 +2,20 @@
  * @Author: tanzhiyu
  * @Date: 2021-10-07 23:03:59
  * @LastEditors: tanzhiyu
- * @LastEditTime: 2021-11-01 17:07:08
+ * @LastEditTime: 2021-11-01 18:20:04
  */
 const fs = require('fs');
-import { Primitive, isPrimitive } from 'utility-types';
 const path = require('path');
+const process = require("process");
+
+const cwd = process.cwd();
 
 export const sayHello = (): string => {
-	console.log('sayHello');
 	return 'sayHello';
 };
 
-const consumer = (value: Primitive | Primitive[]):any => {
-	if (isPrimitive(value)) {
-		return console.log('Primitive value: ', value);
-	}
-	// type of value now inferred as Primitive[]
-	 value.map(primitive => consumer(primitive));
-};
-console.log(consumer(['a', 'b', 'c', 1, 2]));
-
 export const writeDataFile = (data: any) => {
-  fs.writeFileSync(path.join(__dirname, 'data.json'), JSON.stringify(data));
+  fs.writeFileSync(path.join(cwd, 'data.json'), JSON.stringify(data));
 }
 
 
